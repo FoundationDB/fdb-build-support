25bded231b08ec5e9f308482a1954e497dbca0cb
Johannes Scheuermann <johscheuer@users.noreply.github.com>
2025-06-30 13:17:16 +0200

    Add multi-arch support for the build image (#93)
    * Add multi-arch support for the build image

--- docker/rockylinux9/dockerd-entrypoint.sh
+++ docker/rockylinux9/dockerd-entrypoint.sh
@@ -22,5 +22,22 @@ done
 # Set buildx as the default builder to ensure we use buildkit:
 # https://docs.docker.com/build/builders/
 docker buildx install
+# Install a custom builder with buildx to support multi-arch images:
+# https://docs.docker.com/build/building/multi-platform/#prerequisites
+docker buildx create \
+  --name container-builder \
+  --driver docker-container \
+  --driver-opt '"env.http_proxy='$HTTP_PROXY'"' \
+  --driver-opt '"env.https_proxy='$HTTPS_PROXY'"' \
+  --driver-opt '"env.no_proxy='$NO_PROXY'"' \
+  --bootstrap \
+  --use
+
+# https://hub.docker.com/r/tonistiigi/binfmt used to install qemu for multi-arch
+# support: https://docs.docker.com/build/building/multi-platform/#qemu.
+docker run --privileged --rm docker.io/tonistiigi/binfmt:qemu-v9.2.2 --install all
+
+# Enable to ensure that the newly created builder is used per default when running docker build.
+# export BUILDX_BUILDER=container-builder
 
 eval "$@"
